73b9dda194e3d574c92250b98f439be5d41fc658
Alex.LastSurvivour <last.sur.90@gmail.com>
2024-03-07 22:55:13 +0500

    ound the solutions to the task - one option left to add str input

--- Program.cs
+++ Program.cs
@@ -1,9 +1,6 @@
 ﻿//the list of methods used
 
-// Method to identify amount of symbols in each element of string array
-using System.Xml.XPath;
-
-void CalculateStringLengths(string[] stringsArray)
+void StringLengths(string[] stringsArray) // working successfully
     {
         // Pick each element separately
         int size = 0;
@@ -19,7 +16,8 @@ void CalculateStringLengths(string[] stringsArray)
             Console.WriteLine($"Строка \"{str}\" содержит {length} символов.");
         }
     }
-int CalculateSizeOfElement (string[] stringsArray)
+
+int CalculateSizeOfFinalArray (string[] stringsArray) // working successfully
     {
         // Pick each element separately
         int size = 0;
@@ -29,42 +27,36 @@ int CalculateSizeOfElement (string[] stringsArray)
             int length = str.Length;
             if (length <= 3)
             {
-               size = size + 1;
+               size++;
             }
         }
         return size;
     }
 
-// string[] CalculateSizeFinalArray (string[] stringsArray)
-//     {
-//         // Pick each element separately
-//         foreach (string str in stringsArray)
-//         {
-//             // Identify the amount of symbols inside of element
-//             string[] Result = new string[CalculateSizeOfElement(stringsArray)];
-//             int index = 0;
-//             for (index = 0; index < CalculateSizeOfElement(stringsArray); index++)
-//             {
-//             Result[index] = str; 
-//             }
-//         }
-//         return Result;
-//     }
+
 void PrintFinalArray (string[] stringsArray)
     {
         // Pick each element separately
-        string[] Result = new string[CalculateSizeOfElement(stringsArray)];
+        string[] Result = new string[CalculateSizeOfFinalArray(stringsArray)];
             int index = 0;
         foreach (string str in stringsArray)
         {
             // Identify the amount of symbols inside of element
-            for (index = 0; index < CalculateSizeOfElement(stringsArray); index++)
+            int length = str.Length;
+            if (length <= 3)
             {
-            Result[index] = str; 
+               Result[index] = str;
+               index++;
             }
+            // for (index = 0; index < CalculateSizeOfFinalArray(stringsArray); index++)
+            // {
+
+            //         Result[index] = str;
+            // }
         }
-        Console.WriteLine($"Новый массив: {string.Join(", ", Result)}");
+        Console.WriteLine($"Новый массив: [ {string.Join(", ", Result)} ]");
     }
+
 
 
 
@@ -72,15 +64,15 @@ void PrintFinalArray (string[] stringsArray)
 // Solution below
 
 // beforegiven examples arrays - uncomment only one line below
-string [] stringsArray = {"Hello", "2", "world", ":-)"}; // alternatively
+// string [] stringsArray = {"Hello", "2", "world", ":-)"}; // alternatively
 // string [] stringsArray = {"1234", "1567", "-2", "computer science"}; // alternatively
+string [] stringsArray = {"1234", "1567", "67", "15", "156","-2", "computer science"}; // alternatively
 // string [] stringsArray = {"Russia", "Denmark", "Kazan"};// alternatively
 
 // alternatively create array filled via console
 
  // Вызываем метод для определения количества символов в каждом элементе массива
-        CalculateStringLengths(stringsArray);
-        int size = CalculateSizeOfElement(stringsArray);
-Console.WriteLine($"Новый массив содежит {CalculateSizeOfElement(stringsArray)} элементов.");
+StringLengths(stringsArray);
+Console.WriteLine($"Новый массив содежит {CalculateSizeOfFinalArray(stringsArray)} элементов.");
 PrintFinalArray(stringsArray);
 
